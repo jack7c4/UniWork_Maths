7d263aa5dc88fa6cb7bf9ef9fa6ced9999345696
John Gutteridge <P1223205X@GH582-007.lec-admin.dmu.ac.uk>
2014-01-10 11:29:02 +0000

    Correction in main function.

--- maths.c
+++ maths.c
@@ -7,9 +7,9 @@ int maths_add(int a, int b) {
 
 int main() {
 
-        printf("5 add 7:  %d\n", maths_add(5,6);
-        //printf("12 subtract 4:  %d\n", maths_subtract(5,6);
-        //printf("6 multiply 3:  %d\n", maths_multiply(5,6);
-        //printf("14 divide 6:  %d\n", maths_divide(5,6);
+        printf("5 add 7:  %d\n", maths_add(5,6));
+        //printf("12 subtract 4:  %d\n", maths_subtract(5,6));
+        //printf("6 multiply 3:  %d\n", maths_multiply(5,6));
+        //printf("14 divide 6:  %d\n", maths_divide(5,6));
         return 0;
 }
