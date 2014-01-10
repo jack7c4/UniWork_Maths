08910501c616514df9a4528b790ba1f74851ca2f
John Gutteridge <P1223205X@GH582-007.lec-admin.dmu.ac.uk>
2014-01-10 11:30:58 +0000

    Added divide function.

--- maths.c
+++ maths.c
@@ -15,11 +15,16 @@ int maths_multiply(int a, int b) {
         return a * b;
 }
 
+int maths_divide(int a, int b) {
+
+        return a / b;
+}
+
 int main() {
 
         printf("5 add 7:  %d\n", maths_add(5,7));
         printf("12 subtract 4:  %d\n", maths_subtract(12,4));
         printf("6 multiply 3:  %d\n", maths_multiply(6,3));
-        //printf("14 divide 6:  %d\n", maths_divide(14,6));
+        printf("14 divide 6:  %d\n", maths_divide(14,6));
         return 0;
 }
