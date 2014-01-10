1a3b04fa7b8b008b302a13113894faeec618225d
John Gutteridge <P1223205X@GH582-007.lec-admin.dmu.ac.uk>
2014-01-10 11:30:09 +0000

    Added subtract function.

--- maths.c
+++ maths.c
@@ -5,11 +5,16 @@ int maths_add(int a, int b) {
         return a + b;
 }
 
+int maths_subtract(int a, int b) {
+
+        return a - b;
+}
+
 int main() {
 
-        printf("5 add 7:  %d\n", maths_add(5,6));
-        //printf("12 subtract 4:  %d\n", maths_subtract(5,6));
-        //printf("6 multiply 3:  %d\n", maths_multiply(5,6));
-        //printf("14 divide 6:  %d\n", maths_divide(5,6));
+        printf("5 add 7:  %d\n", maths_add(5,7));
+        printf("12 subtract 4:  %d\n", maths_subtract(12,4));
+        //printf("6 multiply 3:  %d\n", maths_multiply(6,3));
+        //printf("14 divide 6:  %d\n", maths_divide(14,6));
         return 0;
 }
