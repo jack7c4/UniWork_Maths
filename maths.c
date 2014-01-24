5a7f71bd9a4bae75ee1931b403cffb41fc53375e
John Gutteridge <P1223205X@GH582-017.lec-admin.dmu.ac.uk>
2014-01-24 11:46:53 +0000

    Adde mod function.

--- maths.c
+++ maths.c
@@ -20,12 +20,18 @@ int maths_divide(int a, int b) {
         return a / b;
 }
 
+int maths_mod(int a, int b) {
+
+        return a % b;
+}
+
 int main() {
 
         printf("5 add 7:  %d\n", maths_add(5,7));
         printf("12 subtract 4:  %d\n", maths_subtract(12,4));
         printf("6 multiply 3:  %d\n", maths_multiply(6,3));
         printf("14 divide 6:  %d\n", maths_divide(14,6));
+        printf("11 mod 24:  %d\n", maths_mod(11,24));
 
         int ns[5];
         printf("\nPlease enter five numbers:  ");
