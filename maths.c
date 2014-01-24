dda96e31d2a8d0b2671a3f6e0a62dd22061c8652
John Gutteridge <P1223205X@GH582-017.lec-admin.dmu.ac.uk>
2014-01-24 11:50:22 +0000

    Added trivial change.

--- maths.c
+++ maths.c
@@ -20,6 +20,11 @@ int maths_divide(int a, int b) {
         return a / b;
 }
 
+int maths_mod(int a, int b) {
+
+        return a % b;
+}
+
 int main() {
 
         // adding trivial change
@@ -27,6 +32,7 @@ int main() {
         printf("12 subtract 4:  %d\n", maths_subtract(12,4));
         printf("6 multiply 3:  %d\n", maths_multiply(6,3));
         printf("14 divide 6:  %d\n", maths_divide(14,6));
+        printf("11 mod 24:  %d\n", maths_mod(11,24));
 
         int ns[5];
         printf("\nPlease enter five numbers:  ");
